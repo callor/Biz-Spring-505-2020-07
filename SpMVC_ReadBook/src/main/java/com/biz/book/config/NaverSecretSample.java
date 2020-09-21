2f5075247b645657dbf17e7ce79d7eb1db964060
callor <callor@callor.com>
2020-09-21 10:18:38 +0900

    ReadBook

---
+++ SpMVC_ReadBook/src/main/java/com/biz/book/config/NaverSecretSample.java
@@ -0,0 +1,13 @@
+package com.biz.book.config;
+
+public class NaverSecretSample {
+	
+	/*
+	 * 네이버 Open API 등록을 수행한 후
+	 * Client ID 와 Client Secret를 복사하여 등록한 후
+	 * 이 클래스를 NaverSecret로 변경하여 프로젝트를 실행하시오
+	 */
+	public final static String NAVER_CLIENT_ID = "YOUR NAVER CLIENT ID";
+	public final static String NAVER_CLIENT_SECRET = "YOUR NAVER SECRET";
+	
+}
